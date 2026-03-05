7c3b1c97a05130f31ae6cb8e543d9e05ad7fe82a
Mat <matthewj0123@outlook.com>
2026-03-05 21:17:41 +0800

    add script

--- test.py
+++ test.py
@@ -1,12 +1,21 @@
 #This file has NO relation with function
 #Solely for myself testing new libraries
-import pandas
+import pandas as pd
 import torch
 import jieba
 import tqdm
 
-test_data = pandas.read_csv('data/SentimentData.csv')
-print(test_data.head())
-print(test_data.columns)
-print(test_data.shape)
-print(test_data["label"].value_counts())
+def PrintCSV() -> None:
+    test_data = pandas.read_csv('data/SentimentData.csv')
+    print(test_data.head())
+    print(test_data.columns)
+    print(test_data.shape)
+    print(test_data["label"].value_counts())
+
+def CutVocabulary() -> None:
+    test_data = pandas.read_csv('data/SentimentData.csv')
+    print(test_data.loc[1:3, "text":"label"])
+
+t = pd.read_csv('data/SentimentData.csv')
+for i in range(0, len(t)):
+    print(list(jieba.cut(t["text"][i])))
