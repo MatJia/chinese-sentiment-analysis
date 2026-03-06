c28ded64c644040565f819a850b53d0ec426a808
Matthew <matthewj0123@outlook.com>
2026-03-06 16:10:52 +1100

    all good

--- source_code/model.py
+++ source_code/model.py
@@ -14,3 +14,23 @@ class Sentiment(nn.Module):
         x = x.mean(dim=1)
         x = self.fc(x)
         return x
+
+if __name__ == "__main__":
+    model = Sentiment(len(vocab), 16, 4)
+    x = torch.tensor(train_padded_list)
+    y = torch.tensor(train_label_list)
+    loss_fn = nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+    for epoch in range(224):
+        opt = model(x)
+        loss = loss_fn(opt, y)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        print(f"epoch: {epoch}, loss: {loss.item()}")
+    x_test = torch.tensor(predict_padded_list)
+    y_test = torch.tensor(predict_label_list)
+    opt_test = model(x_test)
+    pred = torch.argmax(opt_test, dim = 1)
+    print(pred)
+    print(y_test)
