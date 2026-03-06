bc0b11fe03824028931383ea51a72410ce94e1a8
Matthew <matthewj0123@outlook.com>
2026-03-06 14:37:17 +1100

    all good

--- source_code/model.py
+++ source_code/model.py
@@ -4,7 +4,13 @@ import torch.nn as nn
 
 vocab, word_frq, train_size, train_padded_list, train_label_list, predict_padded_list, predict_label_list = load_set()
 
-class sentiment(nn.Module)
-    def __init__(self):
+class Sentiment(nn.Module):
+    def __init__(self, vocab_len, embed_len, class_len):
         super().__init__()
-    def forward(self):
+        self.embedding = nn.Embedding(vocab_len, embed_len)
+        self.fc = nn.Linear(embed_len, class_len)#fully connect
+    def forward(self, x):
+        x = self.embedding(x)
+        x = x.mean(dim=1)
+        x = self.fc(x)
+        return x
